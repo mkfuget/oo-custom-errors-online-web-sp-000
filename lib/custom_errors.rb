b09521994db5033837fd404c979725b60da1bec5
Michael Fuget <michaelfuget@gmail.com>
2020-12-30 08:24:44 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,8 +7,24 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != person 
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
   end
+
+  class PartnerError < StandardError
+      def message
+    "you must give the get_married method an argument of an instance of the person class! You done Fucked up now"
+  end
+
+  end
+
 
 end
 
